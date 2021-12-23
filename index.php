d6eb853e73678213f0b6badc3167608969b6ce43
Yemi <olayemey@gmail.com>
2021-12-23 10:31:26 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>walletApp</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+	<div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="col-md-4 m-auto p-4">
+					<div class="card">
+						<div class="card-head bg-primary">
+							<p class="text-justify">
+								Wallet App
+							</p>
+						</div>
+							<div class="card-body">
+								<div class="form">
+									<form method="POST">
+										<label class="label">click to create your wallet</label>
+										<button class="btn btn-link">create wallet</button>
+										
+									</form>
+									
+								</div>
+								
+							</div>
+							
+						
+						
+					</div>
+					
+				</div>
+
+			
+			</div>
+		
+
+		</div>
+	</div>
+
+
+
+
+
+
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="bootstrap/jquery.min.js"></script>
+</body>
+</html>
